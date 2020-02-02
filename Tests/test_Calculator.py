bc52c4d10a7db4f0527de7874e83b37ede83acdc
Duttp1998 <50345085+Duttp1998@users.noreply.github.com>
2020-02-02 16:50:08 -0500

    Update test_Calculator.py

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -13,7 +13,32 @@ class MyTestCase(unittest.TestCase):
     def test_calculator_addition(self):
         calculator = Calculator()
         result = calculator.addition(1, 2)
-        self.assertEquals(3, result)
+        self.assertEqual(3, result)
+
+    def test_calculator_subtraction(self):
+        calculator = Calculator()
+        result = calculator.subtraction(1, 2)
+        self.assertEqual(-1, result)
+
+    def test_calculator_multiplication(self):
+        calculator = Calculator()
+        result = calculator.multiplication(1, 2)
+        self.assertEqual(2, result)
+
+    def test_calculator_division(self):
+        calculator = Calculator()
+        result = calculator.division(1, 2)
+        self.assertEqual(.5, result)
+
+    def test_calculator_square(self):
+        calculator = Calculator()
+        result = calculator.square(1, 2)
+        self.assertEqual(1, result)
+
+    def test_calculator_root(self):
+        calculator = Calculator()
+        result = calculator.root(1)
+        self.assertEqual(1, result)
 
 
 if __name__ == '__main__':
