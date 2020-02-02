e369e6db63a68329e6dad14f8264c1bb4766b9f0
Duttp1998 <50345085+Duttp1998@users.noreply.github.com>
2020-02-02 16:48:37 -0500

    Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Calculator:
 
     def __init__(self):
@@ -5,3 +8,18 @@ class Calculator:
 
     def addition(self, a, b):
         return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        return a / b
+
+    def square(self, a, b):
+        return a ** b
+
+    def root(self, a):
+        return math.sqrt(a)
